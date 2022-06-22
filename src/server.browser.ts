21ca46942c81200f5f997a4b165b86e64fe45f36
achingbrain <alex@achingbrain.net>
2022-06-22 08:17:03 +0100

    chore: linting

--- src/server.browser.ts
+++ src/server.browser.ts
@@ -1,3 +1,5 @@
-export function createServer() {
+import type { ServerOptions, WebSocketServer } from './server.js'
+
+export function createServer (opts?: ServerOptions): WebSocketServer {
   throw new Error('WebSocket Servers cannot run in browsers')
 }
